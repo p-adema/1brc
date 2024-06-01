4395ef620e74d5ddb7b94eb89fad72c97f5f96c9
p-adema <117938385+p-adema@users.noreply.github.com>
2024-06-01 13:46:18 +0200

    Used more iterators, added MacOS target ignore

--- src/worker.rs
+++ src/worker.rs
@@ -1,8 +1,8 @@
 use std::fmt::{Display, Formatter};
-use std::sync::{Arc, mpsc, Mutex};
-use std::thread::JoinHandle;
 use std::fs::File;
 use std::io::Read;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread::JoinHandle;
 
 use crate::ref_hash_map::RefHashMap;
 
@@ -14,29 +14,27 @@ pub(crate) struct Buffers(Vec<ThreadBuffer>);
 pub(crate) type ThreadBuffer = Arc<[Mutex<[u8; BLOCK_SIZE]>; N_BLOCKS]>;
 pub(crate) type RefMap = RefHashMap<Vec<u8>, Station>;
 
-fn parse_worker(
-    stop_rx: mpsc::Receiver<()>,
-    thread_buffer: ThreadBuffer,
-) -> RefMap {
+fn parse_worker(stop_rx: mpsc::Receiver<()>, thread_buffer: ThreadBuffer) -> RefMap {
     let mut map: RefMap = RefHashMap::with_capacity(512);
-    for i in (0..N_BLOCKS).cycle() {
-        if i == 0 && stop_rx.try_recv().is_ok() {
-            break;
-        }
-
-        let buf = thread_buffer[i].try_lock().ok();
-        if buf.is_none() {
-            continue;
-        }
-        let mut buf = buf.unwrap();
-        if buf[0] == 0 {
-            drop(buf);
-            std::thread::sleep(std::time::Duration::new(0, 50));
-            continue;
-        }
-        buf_parse(&mut map, buf.as_slice());
-        buf[0] = 0;
-    }
+    (0..N_BLOCKS)
+        .cycle()
+        .map_while(|i| {
+            (i != 0 || stop_rx.try_recv().is_err()).then(|| thread_buffer[i].try_lock().ok())
+        })
+        .flatten()
+        .filter_map(|buf| {
+            if buf[0] == 0 {
+                drop(buf);
+                std::thread::sleep(std::time::Duration::new(0, 50));
+                None
+            } else {
+                Some(buf)
+            }
+        })
+        .for_each(|mut buf| {
+            buf_parse(&mut map, buf.as_slice());
+            buf[0] = 0;
+        });
     // last pass
     for buf in (0..N_BLOCKS)
         .map(|i| thread_buffer[i].lock().unwrap())
@@ -75,32 +73,38 @@ impl Parsers {
                 (map_handle, stop_tx)
             })
             .unzip();
-        Self { thread_handles, stop_handles }
+        Self {
+            thread_handles,
+            stop_handles,
+        }
     }
 
     pub(crate) fn join(self) -> Vec<(String, Station)> {
-        self.stop_handles.into_iter().for_each(|s| s.send(()).unwrap());
-        let mut res = self.thread_handles
+        self.stop_handles
+            .into_iter()
+            .for_each(|s| s.send(()).unwrap());
+        let mut res = self
+            .thread_handles
             .into_iter()
             .map(|h| h.join().unwrap())
-            .fold(std::collections::HashMap::<String, Station>::with_capacity(512), |mut map, like| {
-                for (name, station2) in like.into_iter() {
-                    let name = String::from_utf8(name).expect("Should be valid UTF-8");
-                    map.entry(name)
-                        .and_modify(|station1| {
-                            station1.update(&station2)
-                        })
-                        .or_insert(station2);
-                }
-                map
-            })
+            .fold(
+                std::collections::HashMap::<String, Station>::with_capacity(512),
+                |mut map, like| {
+                    for (name, station2) in like.into_iter() {
+                        let name = String::from_utf8(name).expect("Should be valid UTF-8");
+                        map.entry(name)
+                            .and_modify(|station1| station1.update(&station2))
+                            .or_insert(station2);
+                    }
+                    map
+                },
+            )
             .into_iter()
             .collect::<Vec<(String, Station)>>();
         res.sort_unstable_by(|(n1, _), (n2, _)| n1.cmp(n2));
         res
     }
 }
-
 
 fn update(map: &mut RefMap, line: Line) {
     map.entry_ref(line.station)
@@ -186,7 +190,11 @@ pub fn read_worker(thread_buffers: Buffers, mut file: File) {
     let mut remainder = [0; 50];
     let mut remainder_size = 0;
     for thread_locks in thread_buffers.0.iter().cycle() {
-        for mut buf in thread_locks.iter().filter_map(|l| l.try_lock().ok()).filter(|b| b[0] == 0) {
+        for mut buf in thread_locks
+            .iter()
+            .filter_map(|l| l.try_lock().ok())
+            .filter(|b| b[0] == 0)
+        {
             let mut start = remainder_size;
             buf[..start].copy_from_slice(&remainder[..remainder_size]);
             loop {
@@ -202,8 +210,7 @@ pub fn read_worker(thread_buffers: Buffers, mut file: File) {
             }
 
             let last_nl = (BLOCK_SIZE - 50)
-                + memchr::memrchr(b'\n', &buf[BLOCK_SIZE - 50..])
-                .expect("Missing newline in file");
+                + memchr::memrchr(b'\n', &buf[BLOCK_SIZE - 50..]).expect("Missing newline in file");
 
             let rem = &buf[last_nl + 1..];
             remainder_size = rem.len();
@@ -211,7 +218,6 @@ pub fn read_worker(thread_buffers: Buffers, mut file: File) {
         }
     }
 }
-
 
 impl Buffers {
     pub(crate) fn new(parse_threads: usize) -> Self {
@@ -226,7 +232,7 @@ impl Buffers {
                             .unwrap(),
                     )
                 })
-                .collect::<Vec<_>>()
+                .collect::<Vec<_>>(),
         )
     }
 
