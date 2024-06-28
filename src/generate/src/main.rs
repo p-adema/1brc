cf8559a9c58ce53ae522613958d622303d735eec
p-adema <117938385+p-adema@users.noreply.github.com>
2024-06-28 16:26:41 +0200

    Add code for generating data

---
+++ src/generate/src/main.rs
@@ -0,0 +1,21 @@
+mod stations;
+
+use std::io::Write;
+use rand::prelude::*;
+use stations::STATIONS;
+
+const TOTAL: usize = 1_000_000_000;
+const TEMPERATURE_STD: f64 = 10.;
+
+fn main() {
+    
+    let mut out = std::io::BufWriter::new(std::fs::File::create_new("measurements.nosync").expect("Measurement file already exists!"));
+    let mut station_rng = thread_rng();
+    let mut temp_rng = thread_rng();
+
+    for &(name, mean) in std::iter::from_fn(|| STATIONS.choose(&mut station_rng)).take(TOTAL) {
+        let measurement: f64 = rand_distr::Normal::new(mean, TEMPERATURE_STD).unwrap().sample(&mut temp_rng);
+        writeln!(out, "{name};{measurement:.1}").unwrap();
+    };
+}
+
